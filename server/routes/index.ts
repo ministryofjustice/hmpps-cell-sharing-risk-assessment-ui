b9efb861f31c6ccd4957667171d966a5d6efed3e
Thource <adam.sharp@justice.gov.uk>
2026-08-11 09:07:19 +0100

    MAPA-231: Move routing

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -8,7 +8,6 @@ import prisonerRouter from './prisonerRouter'
 import adminRouter from './adminRouter'
 import allPrisonersRouter from './allPrisonersRouter'
 import recentArrivalsRouter from './recentArrivalsRouter'
-import csraQuestionController from '../controllers/csraQuestionController'
 
 export default function routes(services: Services): Router {
   const { prisonerSearchService, manageUsersService } = services
@@ -33,17 +32,6 @@ export default function routes(services: Services): Router {
   router.use('/assessments-in-progress', assessmentsInProgressRouter(services))
   router.use('/prisoner/:prisonerNumber', requirePrisonerAccess, prisonerRouter(services))
   router.use('/admin', adminRouter(services))
-
-  router.get(
-    '/prisoner/:prisonerNumber/csra/:assessmentId/section/:sectionId{/:stepId}',
-    requirePrisonerAccess,
-    csraQuestionController({ auditService, csraService }),
-  )
-  router.post(
-    '/prisoner/:prisonerNumber/csra/:assessmentId/section/:sectionId{/:stepId}',
-    requirePrisonerAccess,
-    csraQuestionController({ auditService, csraService }),
-  )
 
   return router
 }
